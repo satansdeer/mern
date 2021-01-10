0f23076352856ed7f4b43f6fb40148398196b23c
Maksim Ivanov <satansdeer@gmail.com>
2021-01-10 19:41:04 +0100

    Add build folder

---
+++ client/build/precache-manifest.d4d6445f49257196dc9a4a9eca6e32ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dc074d7f458434bf6da63f23d791be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad826403d96aa17791c4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e2c18f346803476a9b3",
+    "url": "/static/js/2.91a2d4c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91a2d4c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad826403d96aa17791c4",
+    "url": "/static/js/main.5bc9b37f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
